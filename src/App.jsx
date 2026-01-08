5acb117720574ff3236fdb6a4e6ba6a17e486648
rudra <bhupandertech@gmail.com>
2026-01-08 17:06:14 +0530

    The basic styling and setting up the components are implemented. Further advanced Functionalities like deleting the task is yet to be implemented onto the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function addTodo() {
+    if (!task.trim()) return;
+
+    setTodos([...todos, task]);
+    setTask("");
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex justify-center items-center w-full h-screen">
+        <div className="w-160 max-w-full rounded-xl">
+          <Header />
+          <div>
+            <div className="bg-purple-400 w-full flex justify-center p-4 text-xl">
+              <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                className="border-2"
+                onKeyDown={(e) => {
+                  if(e.key === "Enter") addTodo();
+                }}
+              />
+              <button
+                className="border-2 bg-blue-400 rounded-md p-2"
+                onClick={addTodo}
+              >
+                Add
+              </button>
+            </div>
+          </div>
+          <TodoList taskData={todos} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+
+    <div className='rounded-xl flex justify-center items-center bg-purple-500'>
+      <h1 className='m-5 text-5xl'>To Do</h1>
+    </div>
+  )
+}
+
+export default Header
