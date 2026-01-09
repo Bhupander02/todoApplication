b11536c35f663b8084a135845c162b40b91046e4
rudra <bhupandertech@gmail.com>
2026-01-09 12:08:52 +0530

    Updated the code for text error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,10 @@
-import { useState } from "react";
-
-function TodoItem({ todo, onDelete, onToggle, onEdit, onSave }) {
-  const [editText, setEditText] = useState(todo.text);
-
-  // console.log(props.info);
+import React from "react";
+function Header() {
   return (
-    <li
-      className={`flex outline outline-purple-400 items-center text-2xl justify-between px-3 py-2 rounded-lg ${
-        todo.completed ? "opacity-50 line-through" : ""
-      }`}
-    >
-      {todo.isEditing ? (
-        <input
-          className="flex-1 px-2 py-1 rounded bg-gray-600 outline-none"
-          defaultValue={todo.text}
-          onChange={(e) => setEditText(e.target.value)}
-          onKeyDown={(e) => {
-            e.key === "Enter" && onSave(todo.id, editText);
-          }}
-        />
-      ) : (
-        <span
-          className="cursor-pointer flex-1"
-          onClick={() => onToggle(todo.id)}
-        >
-          {todo.text}
-        </span>
-      )}
-
-      <div className="flex gap-2 ml-3">
-        {todo.isEditing ? (
-          <button className="ml-3" onClick={() => onSave(todo.id, editText)}>
-            Save
-          </button>
-        ) : (
-          <button
-            className="text-red-400 hover:text-red-500"
-            onClick={() => onEdit(todo.id)}
-          >
-            Edit
-          </button>
-        )}
-
-        <button
-          className=" text-red-400 hover:text-red-500"
-          onClick={() => onDelete(todo.id)}
-        >
-          del
-        </button>
-      </div>
-    </li>
+    <div className="flex justify-center items-center bg-gray-700">
+      <h1 className="m-5 text-5xl text-purple-500">To Do</h1>
+    </div>
   );
 }
 
-export default TodoItem;
+export default Header;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,30 +1,54 @@
-import React from "react";
 import { useState } from "react";
 
-function TodoItem(props) {
+function TodoItem({ todo, onDelete, onToggle, onEdit, onSave }) {
+  const [editText, setEditText] = useState(todo.text);
+
   // console.log(props.info);
-  const [done, setDone] = useState(false)
-  function delTask(){
-    
-
-  }
-
-
-
   return (
-    <li 
-    className= {`flex items-center text-2xl justify-between px-3 py-2 rounded-lg ${ done ? "opacity-50 line-through" : "" }`}  >
-      <span
-      className="cursor-pointer flex-1"
-      onClick={() => setDone(!done)}>
-        {props.text}
-      </span>
-      <button 
-      className="ml-3 text-red-400 hover:text-red-500"
-      onClick={delTask}
-      >
-        del
-      </button>
+    <li
+      className={`flex outline outline-purple-400 items-center text-2xl justify-between px-3 py-2 rounded-lg ${
+        todo.completed ? "opacity-50 line-through" : ""
+      }`}
+    >
+      {todo.isEditing ? (
+        <input
+          className="flex-1 px-2 py-1 rounded bg-gray-600 outline-none"
+          defaultValue={todo.text}
+          onChange={(e) => setEditText(e.target.value)}
+          onKeyDown={(e) => {
+            e.key === "Enter" && onSave(todo.id, editText);
+          }}
+        />
+      ) : (
+        <span
+          className="cursor-pointer flex-1"
+          onClick={() => onToggle(todo.id)}
+        >
+          {todo.text}
+        </span>
+      )}
+
+      <div className="flex gap-2 ml-3">
+        {todo.isEditing ? (
+          <button className="ml-3" onClick={() => onSave(todo.id, editText)}>
+            Save
+          </button>
+        ) : (
+          <button
+            className="text-red-400 hover:text-red-500"
+            onClick={() => onEdit(todo.id)}
+          >
+            Edit
+          </button>
+        )}
+
+        <button
+          className=" text-red-400 hover:text-red-500"
+          onClick={() => onDelete(todo.id)}
+        >
+          del
+        </button>
+      </div>
     </li>
   );
 }
